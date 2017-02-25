8443b23ebcfc4b0a3f803c4d535c70c4564a7dcb
Aakif Nazir <webmaster@nixfifty.com>
2017-02-25 21:50:10 +0000

    Create a table to store user feedback
    Signed-off-by: Aakif Nazir <webmaster@nixfifty.com>

--- upload/src/addons/XenTrader/Setup.php
+++ upload/src/addons/XenTrader/Setup.php
@@ -12,4 +12,35 @@ class Setup extends AbstractSetup
 	use StepRunnerInstallTrait;
 	use StepRunnerUpgradeTrait;
 	use StepRunnerUninstallTrait;
+
+	public function installStep1()
+	{
+		$db = $this->db();
+		$schemaManager = $db->getSchemaManager();
+		$defaultTableConfig = $schemaManager->getTableConfigSql();
+
+		$this->query("
+	        CREATE TABLE `xf_xentrader_feedback` (
+	            `feedback_id` INT(10) NOT NULL AUTO_INCREMENT,
+            	`from_user_id` INT(10) UNSIGNED NOT NULL,
+            	`from_username` VARCHAR(50) NOT NULL,
+            	`to_user_id` INT(10) UNSIGNED NOT NULL,
+            	`to_username` VARCHAR(50) NOT NULL,
+            	`thread_id` INT(10) NOT NULL,
+            	`thread_title` VARCHAR(150) NOT NULL,
+            	`feedback` VARCHAR(80) NOT NULL,
+            	`rating` TINYINT(1) NOT NULL,
+            	`type` ENUM('buy','sell','trade') NOT NULL,
+            	`feedback_date` INT(10) UNSIGNED NOT NULL,
+	            PRIMARY KEY (`feedback_id`)
+	        ) {$defaultTableConfig}
+    	");
+	}
+
+	public function uninstallStep1()
+	{
+		$this->query("
+	        DROP TABLE `xf_xentrader_feedback`;
+    	");
+	}
 }
